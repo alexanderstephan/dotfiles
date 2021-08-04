651cb7d6afa3ca3432e64f3af8cee111d8411af2
Alexander Stephan <alexander.stephan@tum.de>
2021-08-04 23:51:23 +0200

    Add sudo !!

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,14 @@
 set -gx EDITOR nvim
 fish_vi_key_bindings
 source ~/.alias
+function fish_mode_prompt
+  # Turns off mode indicator
+end
+
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+    eval command sudo $history[1]
+else
+    command sudo $argv
+    end
+end
